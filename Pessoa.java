ef41b8312166b597ca3ddfa3524db5078edc4388
nandadantas <97527712+nandadantas@users.noreply.github.com>
2022-01-11 11:26:05 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,29 @@
+pacote  com.teste ;
+
+ classe  pública Pessoa {
+	
+	 nome da string privada ;
+	private  String lastName;
+
+	public  Person ( String  name , String  lastName ) {
+		isso . nome = nome;
+		isso . lastName = lastName;
+	}
+
+	public  String  getName () {
+		nome de retorno ;
+	}
+
+	public  void  setName ( String  name ) {
+		isso . nome = nome;
+	}
+
+	public  String  getLastName () {
+		return sobrenome;
+	}
+
+	public  void  setLastName ( String  lastName ) {
+		isso . lastName = lastName;
+	}
+
+}
